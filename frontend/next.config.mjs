4271c2cde10e3a8ce38ec24b4e81e00a073dd1ba
Leonardo Nascimento <lsncwb@gmail.com>
2024-06-29 18:18:59 -0300

    chore: next settings

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+  }
+  
+  module.exports = {
+    nextConfig,
+    images: {
+      domains: ['imgur.com', 'i.imgur.com', 'fakestoreapi.com'],
+    },
+  }
+  
