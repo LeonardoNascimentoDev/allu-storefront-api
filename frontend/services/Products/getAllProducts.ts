8bbfb1af8044be99a8684c8ed6324539fbeb6220
Leonardo Nascimento <lsncwb@gmail.com>
2024-06-29 16:41:14 -0300

    feat: creating services to consume endpoints

---
+++ frontend/services/Products/getAllProducts.ts
@@ -0,0 +1,12 @@
+import { Dispatch, SetStateAction } from 'react'
+import { Product } from '../../types/Product'
+
+async function getAllProducts(
+  setProducts: Dispatch<SetStateAction<Product[]>>
+) {
+  fetch('http://localhost:3000/products')
+    .then((res) => res.json())
+    .then((json) => setProducts(json))
+}
+
+export default getAllProducts
---
+++ frontend/services/Products/getProductsByCategory.ts
@@ -0,0 +1,14 @@
+import { Dispatch, SetStateAction } from 'react'
+import { Product } from '../../types/Product'
+
+async function getProductsByCategory(
+  setProducts: Dispatch<SetStateAction<Product[]>>,
+  name: string,
+  categoryName: string
+) {
+  fetch(`http://localhost:3000/products/category?${categoryName}?name${name}`)
+    .then((res) => res.json())
+    .then((json) => setProducts(json))
+}
+
+export default getProductsByCategory
