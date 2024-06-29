f78a4c2191d7f57f4379614876407c2fdac20095
Leonardo Nascimento <lsncwb@gmail.com>
2024-06-29 18:10:42 -0300

    feat:creating navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import { RootStateOrAny, useSelector } from 'react-redux'
+import { Nav } from '../../styles/components/Navbar'
+
+interface Product {
+  quantity: string
+}
+
+function Navbar(): JSX.Element {
+  const cart = useSelector((state: RootStateOrAny) => state.cart)
+
+  const getItemsCount = () => {
+    return cart.reduce(
+      (accumulator: number, item: Product) => accumulator + item.quantity,
+      0
+    )
+  }
+
+  return (
+    <Nav>
+      <p className="logo">Allu</p>
+      <ul className="links">
+        <li className="navlink">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="navlink">
+          <Link href="/shop">Catalogo</Link>
+        </li>
+        <li className="navlink">
+          <Link href="/cart">{`Cart (${getItemsCount()})`}</Link>
+        </li>
+      </ul>
+    </Nav>
+  )
+}
+
+export default Navbar
