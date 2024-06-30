c21629583ba9fed3b915b4403c13821e1aaeefd0
Leonardo Nascimento <lsncwb@gmail.com>
2024-06-30 16:22:17 -0300

    feat: adding service to search for product by id

---
+++ frontend/src/services/Products/getProductById.ts
@@ -0,0 +1,23 @@
+import { Dispatch, SetStateAction } from "react";
+import { Product } from "../../types/Product";
+
+async function getProductById(
+  setProducts: Dispatch<SetStateAction<Product[]>>,
+  id: string
+) {
+  try {
+    const url = `http://localhost:4000/products/${id}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch product.");
+    }
+
+    const json = await response.json();
+    setProducts(json);
+  } catch (error) {
+    console.error("Error fetching product:", error);
+  }
+}
+
+export default getProductById;
